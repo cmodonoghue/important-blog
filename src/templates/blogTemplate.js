71e1a02f0744bc853f7dd6cece8172701eecb7a5
Christian <odonoghuecm@gmail.com>
2019-02-26 23:58:28 -0600

    style mobile post page

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -18,12 +18,13 @@ export default function Template({
       <Navbar />
       <div className="blog-post-container"> 
         <div className="blog-post">
-        <img className="blog-post-img" src={frontmatter.image}></img>
+          <img className="blog-post-img" src={frontmatter.image}></img>
           <div className="blog-post__header">            
             <h2 className="blog-post-category">{frontmatter.category}</h2>
             <h1 className="blog-post-title">{frontmatter.title}</h1>
             <h2 className="blog-post-date">{frontmatter.date}</h2>
           </div>
+          <h2 className="blog-post-author">By Christian O'Donoghue</h2>
           <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: html }}
